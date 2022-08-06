5f19f4b011a6d3ed73ef8a0d37d0bef6bec8854f
unknown <yureve5772@gmail.com>
2022-08-06 20:00:20 +0900

    mission5 수행

--- yunalee/js/main.js
+++ yunalee/js/main.js
@@ -3,6 +3,31 @@ const postingBtnByClass =  document.getElementsByClassName("posting_btn");
 const commentList = document.getElementsByClassName("feed_comment_list");
 
 
+fetch("./data/comments.json")
+    .then((res) => res.json())
+    .then((data) =>{
+        //데이터를 받아서 배열로 다시 할당하기                             
+       let commentArray = data.comments;
+       //배열에 담긴 댓글을 하나하나 반복문 돌기
+       commentArray.forEach((comment)=>{
+        //먼저 commentsList 반복문을 돌면서 피드 하나하나에 댓글을 하나씩 넣어주기
+        for (let i = 0; i < commentArray.length; i++) {
+            //댓글을 넣을 div
+            const commentElem = document.createElement("div");
+            //댓글 닉네임을 넣을 span
+            const nameSpan = document.createElement("span");
+            nameSpan.className = "comment-name";
+            nameSpan.textContent = comment.username;
+            //댓글 내용을 넣어주기 
+            const content = comment.content;
+            commentElem.append(nameSpan, content);
+            //피드 하나하나에 댓글 넣기 
+            commentList[i].append(commentElem);
+        }
+
+       });
+    })
+
 Array.from(postingBtnByClass).forEach((button, index )=> {
     button.addEventListener("click",() => {
         const content = commentInputByClass[index].value;
